e624ed0ad837b44d8e0637905f58383fed5b917e
green <41323182+greeeen-dev@users.noreply.github.com>
2025-07-30 20:58:59 +0200

    Improve cursor movement for PiP

--- natsumi/scripts/pip.uc.mjs
+++ natsumi/scripts/pip.uc.mjs
@@ -3,38 +3,56 @@
 // @ignorecache
 // ==/UserScript==
 
+// Adjust this as needed
+const movementMultiplier = 0.7;
+
 function movePictureInPicture(event) {
+    // Get current screen position
     const currentX = screenX;
     const currentY = screenY;
 
+    // Get relative position of mouse within window
+    const mouseRelativeX = event.pageX;
+    const mouseRelativeY = event.pageY;
+
+    // Get total movable area
     const movableX = screen.availWidth - window.innerWidth;
     const movableY = screen.availHeight - window.innerHeight;
 
-    const nativePixelRatio = window.devicePixelRatio || 1;
-    const nativeAvailableX = window.innerWidth * nativePixelRatio;
-    const nativeAvailableY = window.innerHeight * nativePixelRatio;
-
-    const width = event.pageX;
-    const height = event.pageY;
-
-    const movementMultiplier = 0.7;
+    // Calculate movement based on scroll distance
     const movedX = Math.round(event.wheelDeltaX * movementMultiplier);
     const movedY = Math.round(event.wheelDeltaY * movementMultiplier);
 
+    // Calculate new positions for window
     const newX = Math.min(
         Math.max(currentX + movedX, 0), movableX
     );
     const newY = Math.min(
         Math.max(currentY + movedY, 0), movableY
     );
-    const nativeNewX = newX * nativePixelRatio + (width * nativePixelRatio);
-    const nativeNewY = newY * nativePixelRatio + (height * nativePixelRatio);
 
-    console.log(`Moving Picture-in-Picture to: (${newX}, ${newY}) (w: ${movableX}, h: ${movableY})`);
-    document.querySelector("body").focus();
-    window.windowUtils.sendNativeMouseEvent(nativeNewX, nativeNewY, 3, 0, 0, null);
+    // Calculate new positions for mouse
+    const nativePixelRatio = window.devicePixelRatio || 1;
+    const nativeNewX = newX * nativePixelRatio + (mouseRelativeX * nativePixelRatio);
+    let nativeNewY = newY * nativePixelRatio + (mouseRelativeY * nativePixelRatio);;
 
+    // Move PiP window and mouse
     window.moveTo(newX, newY);
+
+    // Ensure the new Y position has been applied correctly
+    // Otherwise, move mouse based on that (this often happens due to certain OS behaviors)
+    if (newY !== screenY) {
+        nativeNewY = screenY * nativePixelRatio + (mouseRelativeY * nativePixelRatio);
+    }
+
+    // Move mouse position to allow further scrolling
+    if (currentX === screenX && currentY === screenY) {
+        // Nothing changed, so no need to move here
+        // This prevents "locking" the mouse on OSes with smooth scrolling
+        return;
+    }
+
+    window.windowUtils.sendNativeMouseEvent(nativeNewX, nativeNewY, 3, 0, 0, null);
 }
 
 document.addEventListener("wheel", function (e) {
