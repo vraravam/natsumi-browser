58f477dc5bcf27569378717da14afe03456fd4d4
green <41323182+greeeen-dev@users.noreply.github.com>
2025-08-01 09:27:37 +0200

    Pinned tabs icon

---
+++ natsumi/scripts/pinned-tabs-icon.uc.mjs
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @include   main
+// @loadOrder 11
+// @ignorecache
+// ==/UserScript==
+
+let observedTabs = [];
+
+function copyTabIcon(tabObject) {
+    if (tabObject.attributes["image"]) {
+        // Get image and set it as a variable
+        let image = tabObject.attributes["image"].value;
+        tabObject.style.setProperty("--natsumi-tab-icon", `url("${image}")`);
+    } else {
+        // Use default icon
+        tabObject.style.setProperty("--natsumi-tab-icon", `url("chrome://global/skin/icons/defaultFavicon.svg")`);
+    }
+}
+
+function addMutatorObserver(tabObject) {
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutationRecord) {
+            copyTabIcon(tabObject);
+        });
+    });
+
+    observer.observe(tabObject, { attributes: true });
+    observedTabs.push(observer);
+}
+
+let pinnedTabsContainer = document.querySelector("#pinned-tabs-container");
+
+const pinnedTabsObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutationRecord) {
+        mutationRecord.addedNodes.forEach(function (addedNode) {
+            if (addedNode.nodeName === "tab") {
+                // If the added node is a pinned tab, copy its icon
+                copyTabIcon(addedNode);
+                // Add observer to monitor changes in the tab
+                addMutatorObserver(addedNode);
+            }
+        });
+    })
+});
+
+// Initial copy of icons for already existing pinned tabs
+let pinnedTabs = pinnedTabsContainer.querySelectorAll("tab");
+
+pinnedTabs.forEach(function (tab) {
+    copyTabIcon(tab);
+    addMutatorObserver(tab);
+});
+
+pinnedTabsObserver.observe(pinnedTabsContainer, { childList: true, subtree: true });
