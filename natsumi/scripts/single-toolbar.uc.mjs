e5a516fc55bc66b3b98c10ace46b6adaa4af8539
green <41323182+greeeen-dev@users.noreply.github.com>
2025-08-23 23:11:57 +0200

    Improve single toolbar view

---
+++ natsumi/scripts/single-toolbar.uc.mjs
@@ -0,0 +1,104 @@
+// ==UserScript==
+// @include   main
+// @ignorecache
+// ==/UserScript==
+
+import * as ucApi from "chrome://userchromejs/content/uc_api.sys.mjs";
+
+let hoverTimeout = null;
+let hoveredElements = 0;
+
+function setHover(isWindowButton = false) {
+    let singleToolbarEnabled = false;
+    if (ucApi.Prefs.get("natsumi.theme.single-toolbar").exists()) {
+        singleToolbarEnabled = ucApi.Prefs.get("natsumi.theme.single-toolbar").value;
+    }
+
+    if (!singleToolbarEnabled) {
+        hoveredElements = 0;
+        document.body.removeAttribute("natsumi-bookmarks-hover");
+        return;
+    }
+
+    if (isWindowButton) {
+        let isMac = Services.appinfo.OS.toLowerCase() !== "darwin";
+        let forcedLeft = false;
+
+        if (ucApi.Prefs.get("natsumi.theme.force-window-controls-to-left").exists()) {
+            forcedLeft = ucApi.Prefs.get("natsumi.theme.force-window-controls-to-left").value;
+        }
+
+        if ((isMac || forcedLeft) && isWindowButton) {
+            return;
+        }
+    }
+
+    if (hoverTimeout) {
+        clearTimeout(hoverTimeout);
+    }
+
+    hoveredElements++;
+    document.body.setAttribute("natsumi-bookmarks-hover", "");
+}
+
+function removeHover(isWindowButton = false) {
+    let singleToolbarEnabled = false;
+    if (ucApi.Prefs.get("natsumi.theme.single-toolbar").exists()) {
+        singleToolbarEnabled = ucApi.Prefs.get("natsumi.theme.single-toolbar").value;
+    }
+
+    if (!singleToolbarEnabled) {
+        hoveredElements = 0;
+        document.body.removeAttribute("natsumi-bookmarks-hover");
+        return;
+    }
+
+    if (isWindowButton) {
+        let isMac = Services.appinfo.OS.toLowerCase() !== "darwin";
+        let forcedLeft = false;
+
+        if (ucApi.Prefs.get("natsumi.theme.force-window-controls-to-left").exists()) {
+            forcedLeft = ucApi.Prefs.get("natsumi.theme.force-window-controls-to-left").value;
+        }
+
+        if ((isMac || forcedLeft) && isWindowButton) {
+            return;
+        }
+    }
+
+    hoveredElements--;
+    if (hoveredElements > 0) {
+        return;
+    } else if (hoveredElements < 0) {
+        hoveredElements = 0;
+    }
+
+    hoverTimeout = setTimeout(() => {
+        document.body.removeAttribute("natsumi-bookmarks-hover");
+    }, 1000);
+}
+
+function detectBookmarkHover() {
+    let bookmarksToolbar = document.getElementById("PersonalToolbar");
+    let windowButtonsContainer = document.querySelector(".titlebar-buttonbox-container");
+
+    if (bookmarksToolbar) {
+        bookmarksToolbar.addEventListener("mouseover", (event) => {
+            setHover();
+        });
+        bookmarksToolbar.addEventListener("mouseout", (event) => {
+            removeHover();
+        });
+    }
+
+    if (windowButtonsContainer) {
+        windowButtonsContainer.addEventListener("mouseover", (event) => {
+            setHover(true);
+        });
+        windowButtonsContainer.addEventListener("mouseout", (event) => {
+            removeHover(true);
+        });
+    }
+}
+
+detectBookmarkHover();
