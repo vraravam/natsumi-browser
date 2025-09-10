e64e1d50a127f0f64bc9fd49d049682cc1970a79
green <41323182+greeeen-dev@users.noreply.github.com>
2025-09-10 18:20:18 +0200

    Improve compact mode

--- natsumi/scripts/preferences.uc.mjs
+++ natsumi/scripts/preferences.uc.mjs
@@ -1431,6 +1431,27 @@ const colors = {
     )*/
 }
 
+const compactStyles = {
+    "default": new MCChoice(
+        "default",
+        "Hide both",
+        "Hides both the toolbar and sidebar.",
+        "<div id='compact-both' class='natsumi-mc-choice-image-browser'></div>"
+    ),
+    "toolbar": new MCChoice(
+        "toolbar",
+        "Hide toolbar",
+        "Hides the toolbar only.",
+        "<div id='compact-toolbar' class='natsumi-mc-choice-image-browser'></div>"
+    ),
+    "sidebar": new MCChoice(
+        "sidebar",
+        "Hide sidebar",
+        "Hides the sidebar only.",
+        "<div id='compact-sidebar' class='natsumi-mc-choice-image-browser'></div>"
+    )
+}
+
 const urlbarLayouts = {
     "floating": new MCChoice(
         false,
@@ -1960,6 +1981,39 @@ function addSidebarButtonsPane() {
     prefsView.insertBefore(sidebarButtonsNode, homePane);
 }
 
+function addCompactStylesPane() {
+    let prefsView = document.getElementById("mainPrefPane");
+    let homePane = prefsView.querySelector("#firefoxHomeCategory");
+
+    // Create theme selection
+    let styleSelection = new MultipleChoicePreference(
+        "natsumiCompactStyle",
+        "natsumi.theme.compact-style",
+        "Style",
+        "Choose what you want to hide when Compact Mode is active.."
+    );
+
+    for (let style in compactStyles) {
+        styleSelection.registerOption(style, compactStyles[style]);
+    }
+
+    let styleNode = styleSelection.generateNode();
+
+    // Set listeners for each button
+    let styleButtons = styleNode.querySelectorAll(".natsumi-mc-choice");
+    styleButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            let selectedValue = button.getAttribute("value");
+            console.log("Changing style:", selectedValue);
+            setStringPreference("natsumi.theme.compact-style", selectedValue);
+            styleButtons.forEach(btn => btn.classList.remove("selected"));
+            button.classList.add("selected");
+        });
+    });
+
+    prefsView.insertBefore(styleNode, homePane);
+}
+
 function addSidebarMiniplayerPane() {
     let prefsView = document.getElementById("mainPrefPane");
     let homePane = prefsView.querySelector("#firefoxHomeCategory");
@@ -2291,6 +2345,19 @@ function addPreferencesPanes() {
             <html:h1>Sidebar &amp; Tabs</html:h1>
         </hbox>
     `);
+    let compactModeNode = convertToXUL(`
+        <hbox id="natsumiCompactModeCategory" class="subcategory" data-category="paneNatsumiSettings" hidden="true">
+            <html:h1>Compact Mode</html:h1>
+        </hbox>
+        <groupbox id="natsumiCompactSingleToolbar" data-category="paneNatsumiSettings" hidden="true">
+            <div class="natsumi-settings-info warning">
+                <div class="natsumi-settings-info-icon"></div>
+                <div class="natsumi-settings-info-text">
+                    You need to use Multiple Toolbars layout to change Compact Mode styles.
+                </div>
+            </div>
+        </groupbox>
+    `);
     let miniPlayerNode = convertToXUL(`
         <hbox id="natsumiMiniplayerCategory" class="subcategory" data-category="paneNatsumiSettings" hidden="true">
             <html:h1>Miniplayer</html:h1>
@@ -2335,6 +2402,9 @@ function addPreferencesPanes() {
     prefsView.insertBefore(sidebarNode, homePane);
     addSidebarWorkspacesPane();
     addSidebarButtonsPane();
+
+    prefsView.insertBefore(compactModeNode, homePane);
+    addCompactStylesPane();
 
     prefsView.insertBefore(miniPlayerNode, homePane);
     addSidebarMiniplayerPane();
