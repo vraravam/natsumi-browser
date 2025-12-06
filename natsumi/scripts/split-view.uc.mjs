84b79167446e6e5b36b7ba30c709cd4efdcd5784
green. <41323182+greeeen-dev@users.noreply.github.com>
2025-12-06 20:31:29 +0000

    Add patches for Firefox split view

--- natsumi/scripts/split-view.uc.mjs
+++ natsumi/scripts/split-view.uc.mjs
@@ -32,107 +32,38 @@ SOFTWARE.
 import * as ucApi from "chrome://userchromejs/content/uc_api.sys.mjs";
 
 class NatsumiSplitViewManager {
-    maxSplitSize = 0.8;
-    minSplitSize = 0.2;
-
     constructor() {
-        this.tabBoxNode = null;
-        this.tabPanelsNode = null;
-        this.tabPanelsObserver = null;
-        this.splitterMouseDown = false;
-        this.splitSize = 0.5;
     }
 
     init() {
-        // Get tab panels node
-        this.tabPanelsNode = document.getElementById("tabbrowser-tabpanels");
-        this.tabBoxNode = this.tabPanelsNode.parentNode;
-
-        // Observe attributes
-        this.tabPanelsObserver = new MutationObserver((mutations) => {
-            console.log(mutations);
-            if (this.tabPanelsNode.hasAttribute("split-view")) {
-                this.applySplitView();
-            } else {
-                this.removeSplitView();
-            }
-        });
-        this.tabPanelsObserver.observe(this.tabPanelsNode, {
-            attributes: true,
-            attributeFilter: ["split-view"]
-        });
-    }
-
-    applySplitView() {
-        let existingSplitViewSplitter = document.getElementById("natsumi-split-view-splitter");
-        if (existingSplitViewSplitter) {
-            existingSplitViewSplitter.remove();
-        }
-        let existingSplitViewSpacer = document.getElementById("natsumi-split-view-spacer");
-        if (existingSplitViewSpacer) {
-            existingSplitViewSpacer.remove();
-        }
-
-        // Add splitter
-        let splitViewSplitter = document.createXULElement("splitter");
-        splitViewSplitter.id = "natsumi-split-view-splitter";
-        this.tabBoxNode.append(splitViewSplitter);
-        splitViewSplitter.addEventListener("mousedown", () => {
-            this.splitterMouseDown = true;
-            splitViewSplitter.setAttribute("dragging", "true");
-        });
-        splitViewSplitter.addEventListener("mouseup", () => {
-            this.splitterMouseDown = false;
-            splitViewSplitter.removeAttribute("dragging");
-        });
-        splitViewSplitter.addEventListener("mousemove", (event) => {
-            if (!this.splitterMouseDown) {
+        if (ucApi.Prefs.get("natsumi.browser.disable-splitview-patches").exists()) {
+            if (ucApi.Prefs.get("natsumi.browser.disable-splitview-patches").value) {
+                console.warn("Natsumi Split View patches are disabled. If disabling this fixes split views, please report this.");
                 return;
             }
+        }
 
-            let isVertical = (
-                this.tabPanelsNode.style.flexDirection === "column" ||
-                this.tabPanelsNode.style.flexDirection === "column-reverse"
-            );
+        // Add event listener
+        gBrowser.tabContainer.addEventListener("TabClose", this.onTabClose.bind(this));
+    }
 
-            let totalSize = this.tabBoxNode.getBoundingClientRect().width;
-            let position = event.clientX - this.tabBoxNode.getBoundingClientRect().left;
+    onTabClose(event) {
+        let closedTab = event.target;
 
-            if (isVertical) {
-                totalSize = this.tabBoxNode.getBoundingClientRect().height;
-                position = event.clientY - this.tabBoxNode.getBoundingClientRect().top;
+        if (closedTab.splitview) {
+            for (let tab of closedTab.splitview.tabs) {
+                let linkedPanelNode = document.getElementById(tab.linkedPanel);
+
+                if (linkedPanelNode) {
+                    linkedPanelNode.style.removeProperty("width");
+                    linkedPanelNode.removeAttribute("width");
+                }
             }
-
-            this.splitSize = Math.max(Math.min(position / totalSize, this.maxSplitSize), this.minSplitSize);
-            this.tabBoxNode.style.setProperty("--natsumi-split-size", `${this.splitSize * 100}%`);
-        });
-
-        // Add split view spacer
-        let splitViewSpacer = document.createElement("div");
-        splitViewSpacer.id = "natsumi-split-view-spacer";
-        this.tabPanelsNode.append(splitViewSpacer);
-    }
-
-    removeSplitView() {
-        let splitViewSplitter = document.getElementById("natsumi-split-view-splitter");
-        if (splitViewSplitter) {
-            splitViewSplitter.remove();
         }
-        let splitViewSpacer = document.getElementById("natsumi-split-view-spacer");
-        if (splitViewSpacer) {
-            splitViewSpacer.remove();
-        }
-
-        this.splitSize = 0.5;
     }
 }
 
-let isFloorp = false;
-if (ucApi.Prefs.get("natsumi.browser.type").exists) {
-    isFloorp = ucApi.Prefs.get("natsumi.browser.type").value === "floorp";
-}
-
-if (isFloorp) {
+if (!document.body.natsumiSplitViewManager) {
     try {
         document.body.natsumiSplitViewManager = new NatsumiSplitViewManager();
         document.body.natsumiSplitViewManager.init();
